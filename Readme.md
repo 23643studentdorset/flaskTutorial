19f5e18e503cff773b8d85c383d0e20136e2bfd2
23643@student.dorset-college.ie <23643@student.dorset-college.ie>
2023-04-08 16:18:02 +0100

    login/readme added

---
+++ Readme.md
@@ -0,0 +1,31 @@
+# CA4 - backend - ToDo app Flask
+This is a full stac
+k web application created using Python, Flask, and MongoDB that allows users to perform CRUD operations. The initial version of the application was created by following a [tutorial](https://www.digitalocean.com/community/tutorials/how-to-use-mongodb-in-a-flask-application) on DigitalOcean.
+
+Features
+In addition to the basic CRUD functionality of the initial app, 
+this version of the app includes the following new features:
+ - Persistent loging and register of users 
+ - Change in the models of the db adding created on, a deadline and user to the todo tasks
+ - Some paths and functions are just for the sake of experimenting with Flask
+ 
+---
+
+- [x] 5% - Create working development environment with Python, Flask and MongoDB showing hello world app working.
+- [x] 5% - Follow How To Use MongoDB in a Flask Application | DigitalOcean tutorial to create the Flask ToDo App that stores data in MongoDB.
+- [X] 10% - Document the code with information on what each code item is doing
+- [ ] 10% - Document the testing of the function and the assertions being used.
+- [x] 20% - Create a readme file with screenshots of App working
+- [x] 40% - Extend the App to use a new feature for the ToDo App and/or Use a new Data object for the App
+- [x] 10% - Optional for higher grade:  Extend the App to add login authentication.
+
+---
+
+[Usage video](https://youtu.be/jyftFmNkxfo) - After the video I changed some details in the todo HTML 
+
+---
+![hello.jpg](static%2Fscreenshots%2Fhello.jpg)
+![home.jpg](static%2Fscreenshots%2Fhome.jpg)
+![register.jpg](static%2Fscreenshots%2Fregister.jpg)
+![login.jpg](static%2Fscreenshots%2Flogin.jpg)
+![todo.jpg](static%2Fscreenshots%2Ftodo.jpg)
--- app.py
+++ app.py
@@ -2,33 +2,108 @@ import datetime
 from bson.objectid import ObjectId
 import pymongo
 from markupsafe import escape
-from flask import Flask, render_template, request, url_for, redirect, abort
+from flask import Flask, render_template, request, url_for, redirect, abort, json
+from flask import request, redirect, session
+from flask_bcrypt import Bcrypt
+from functools import wraps
 
 # Create an instance of the Flask class
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 # Connect to a MongoDB Atlas cluster and create a database and a collection
 client = pymongo.MongoClient(
     "mongodb+srv://Admin2:Pass123@atlascluster.cgalopx.mongodb.net/?retryWrites=true&w=majority")
 db = client.flask_db
 todos = db.todos
+users = db.users
+app.secret_key = 'this is a secret key'
+
+# Define a login_required decorator to require authentication for certain routes
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get('user') is None:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+# Define a route for the registration form
+@app.route('/register/')
+def register():
+    return render_template('register.html')
+
+# Define a route for registering a new user
+@app.route('/register/', methods=['POST'])
+def register_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    existing_user = users.find_one({'email': email})
+
+    # Check if user already exists
+    if existing_user is None:
+        hashpass = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        # Insert user into database
+        users.insert_one({'email': email, 'password': hashpass})
+        return redirect('/login')
+
+    # If user already exists, redirect to registration page with error message
+    return 'That email already exists!'.encode('utf-8')
+
+
+# Define a route for the login form
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+# Define a route for logging in a user
+@app.route('/login/', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = users.find_one({'email': email})
+
+    # Check if user exists and password is correct
+    if user and bcrypt.check_password_hash(user['password'], password):
+
+        # Save user's email address in a session variable
+        session['user'] = user['email']
+        return redirect('/todo/')
+    else:
+        return redirect('/register/')
+
+
+# Define a route for logging out a user
+@app.route('/logout')
+def logout():
+   # remove the username from the session if it is there
+   session.pop('user', None)
+   return redirect('/')
 
 
 # Define a route for adding and displaying todos
 @app.route('/todo/', methods=('GET', 'POST'))
+# Require authentication for this route
+@login_required
 def todo():
     # If the request method is POST, retrieve the content and values from the form and insert them into the todos collection
     if request.method == 'POST':
         content = request.form['content']
         degree = request.form['degree']
-        todos.insert_one({'content': content, 'degree': degree})
+        deadline = request.form['deadline']
+        created_on = datetime.datetime.utcnow()
+        user = session['user']
+        todos.insert_one({'content': content, 'degree': degree, 'user': user, 'created_on': created_on, 'deadline': deadline})
 
         # Redirect the user back to 'todo' route
         return redirect(url_for('todo'))
 
-    # If the request method is GET, retrieve all todos from the todos collection and pass them to the todo.html template for rendering
-    all_todos = todos.find()
-    return render_template('todo.html', todos=all_todos)
+    # If the request method is GET, retrieve all todos from user in session
+    user = session['user']
+    user_todos = todos.find({'user': user})
+    return render_template('todo.html', todos=user_todos, time_now=datetime.datetime.utcnow())
 
 
 # A route for deleting a todo
@@ -37,12 +112,13 @@ def delete(id):
     todos.delete_one({"_id": ObjectId(id)})
     return redirect(url_for('todo'))
 
-
+# A route for updating a todo
 @app.post('/todo/<id>/update')
 def update(id):
     content = request.form['updated_content']
     degree = request.form['updated_degree']
-    todos.update_one({'_id': ObjectId(id)}, {'$set': {'content': content, 'degree': degree}})
+    deadline = request.form['deadline']
+    todos.update_one({'_id': ObjectId(id)}, {'$set': {'content': content, 'degree': degree, 'deadline': deadline, 'changed_on': datetime.datetime.utcnow()}})
     return redirect(url_for('todo'))
 
 
